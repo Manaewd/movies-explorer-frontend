b743240072eb28bc4be6e3c374a2de288c99c1be
Denis Manaev <manaewd@yandex.ru>
2023-09-21 01:34:35 +0700

    тестю

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -114,7 +114,6 @@ class MainApi {
 
 const mainApi = new MainApi({
   baseUrl: "https://api.manaewdiploma.nomoredomains.xyz",
-  // baseUrl: "http://localhost:3000",
   credentials: "include",
   headers: {
     "Content-Type": "application/json",
